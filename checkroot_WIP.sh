d4f363447b0637610d641078b25a0020c8ab12ba
cnd <cynede@gentoo.org>
2018-11-19 16:19:25 +0400

    sup

--- checkroot_WIP.sh
+++ checkroot_WIP.sh
@@ -1,19 +1,17 @@
 # ROOT CHECK
+CAN_I_RUN_SUDO=$(sudo -n uptime 2>&1|grep "load"|wc -l)
+
+# ROOT CHECK
 checkroot_WIP () {
-	if [ $UID == 0 ]; then
-	echo "Root is detected."
-
-	# Using `sudo`
-	#CONTEPT: elif type sudo &>/dev/null ; then
-	elif [ $UID != 0 ] && [[ -x sudo ]]; then 
-	echo "Avasile needs root permission to proceed, trying using sudo."
-	sudo avasile $@
-
-	# Using `su`
-	elif [ $UID != 0 ]; then 
+	if [[ "$UID" == 0 ]];
+	then
+		echo "Root is detected."
+		avasile "$@"
+	elif [[ "${CAN_I_RUN_SUDO}" -gt 0 ]]; then 
+		echo "Avasile needs root permission to proceed, trying using sudo."
+		sudo avasile "$@"
+	else
 		echo "Avasile needs root permission to proceed, trying to log-in as root."
-		echo DEBUG: avasile $@
-		su root -s "$0" -- "$@"
-		#su -c "avasile $@"   
-fi
+		su -c "avasile $@" -m root
+	fi
 }
